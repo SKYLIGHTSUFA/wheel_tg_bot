129ea997bc4fd447476e53098c290fd9174a3b48
skylights <51323448+SKYLIGHTSUFA@users.noreply.github.com>
2026-01-08 21:27:54 +0300

    [UPD] webapp url

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import json
 import asyncio
 from typing import List, Optional
-
+import logging
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 import aiosqlite
 from aiogram import Bot, Dispatcher, F
@@ -17,7 +17,7 @@ import uvicorn
 BOT_TOKEN = "7854473349:AAEImt52KG7VHaaKzBXwHhEAuB2t94Onukw"
 DB_PATH = os.environ.get("DB_PATH", "db.sqlite3")
 ORDERS_CHAT = "@KolesaUfa02"  # Куда будут приходить уведомления
-WEBAPP_URL = "https://ваш-сайт.onrender.com"  # ВАЖНО: Укажите здесь ваш актуальный URL
+WEBAPP_URL = "https://wheel-tg-bot.onrender.com"  # ВАЖНО: Укажите здесь ваш актуальный 
 
 # Создаем бота глобально, чтобы к нему был доступ из API
 bot = Bot(BOT_TOKEN)
